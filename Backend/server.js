1dcf2c0e43c3cfe8622da2e4afb6b01540fbd167
rounak-O1 <rounakbhardwaj01@gmail.com>
2026-08-19 19:39:48 +0530

    Prepare backend for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,6 @@ const connectToDB = require("./src/config/database")
 connectToDB()
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`)
 })
